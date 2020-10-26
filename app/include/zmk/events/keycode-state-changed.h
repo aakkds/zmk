ae839324cdb9ba67096887c3c4388dad71e9f429
Pete Johanson <peter@peterjohanson.com>
2020-10-26 00:30:24 -0400

    refactor(keys): Unify usage page.
    * Remove need for separate `&cp` behavior, but
      keep it for now for backward compat.
    * Refactor sensor inc/dec as well.

--- app/include/zmk/events/keycode-state-changed.h
+++ app/include/zmk/events/keycode-state-changed.h
@@ -8,6 +8,7 @@
 
 #include <zephyr.h>
 #include <zmk/event-manager.h>
+#include <zmk/hid_usage_pages.h>
 
 struct keycode_state_changed {
     struct zmk_event_header header;
@@ -18,11 +19,23 @@ struct keycode_state_changed {
 
 ZMK_EVENT_DECLARE(keycode_state_changed);
 
-inline struct keycode_state_changed *create_keycode_state_changed(u8_t usage_page, u32_t keycode,
-                                                                  bool state) {
+static inline struct keycode_state_changed *
+create_keycode_state_changed(u8_t usage_page, u32_t keycode, bool state) {
     struct keycode_state_changed *ev = new_keycode_state_changed();
     ev->usage_page = usage_page;
     ev->keycode = keycode;
     ev->state = state;
     return ev;
+}
+
+static inline struct keycode_state_changed *keycode_state_changed_from_encoded(u32_t encoded,
+                                                                               bool pressed) {
+    u16_t page = (encoded >> 16) & 0xFF;
+    u16_t id = encoded & 0xFFFF;
+
+    if (!page) {
+        page = HID_USAGE_KEY;
+    }
+
+    return create_keycode_state_changed(page, id, pressed);
 }
